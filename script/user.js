0118a28f31ee6cbbd6009373e5ca869615d83d46
July_the_month <68832820+july-bit@users.noreply.github.com>
2022-11-14 20:07:41 +0700

    User Page
    Content of User Page

---
+++ script/user.js
@@ -0,0 +1,7 @@
+document.querySelector("#donation_his__button").addEventListener('click', function() {
+    document.querySelector('.modal_background').style.display='flex';
+}); 
+document.querySelector("#close_button").addEventListener('click',
+function() {
+    document.querySelector('.modal_background').style.display='none'; 
+}); 
