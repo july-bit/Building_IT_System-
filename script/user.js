30acf514bb30bac67d5ccba353139ef3949b1df0
July_the_month <68832820+july-bit@users.noreply.github.com>
2022-12-06 13:42:41 +0700

    Debug
    Modify modal form and resize donate button

--- script/user.js
+++ script/user.js
@@ -1,21 +1,25 @@
 document.querySelector("#donation_his__button").addEventListener('click', function() {
     document.querySelector('.modal_background').style.display='flex';
+    document.querySelector('.edit_modal_background').style.display='flex';
 }); 
 document.querySelector("#close_button").addEventListener('click',
 function() {
     document.querySelector('.modal_background').style.display='none'; 
+    document.querySelector('.edit_background').style.display='none'; 
+    document.querySelector('.edit_modal_background').style.display='none'; 
 }); 
-
-document.querySelector(".modal_background").addEventListener('click',
+document.querySelector(".edit_modal_background").addEventListener('click',
 function() {
     document.querySelector('.modal_background').style.display='none'; 
+    document.querySelector('.edit_modal_background').style.display='none'; 
 }); 
-
 document.querySelector(".button_edit").addEventListener('click', function() {
+    document.querySelector('.edit_background').style.display='flex';
     document.querySelector('.edit_modal_background').style.display='flex';
 }); 
 document.querySelector(".edit_modal_background").addEventListener('click',
 function() {
+    document.querySelector('.edit_background').style.display='none'; 
     document.querySelector('.edit_modal_background').style.display='none'; 
 }); 
 
